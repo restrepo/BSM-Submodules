df687fc98c54935bf9b59eee85690493ae49dec6
Diego Restrepo <restrepo@udea.edu.co>
2017-10-11 17:47:56 -0500

    fix compiler

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-cd calchep
+cd SPHENO
+if [ "$(grep -E '\#\s*F90\s*=\s*gfortran' Makefile)" ];then
+    sed -i 's/\# F90 = gfortran/F90 = gfortran/' Makefile
+    sed -ri 's/^F90 = ifort/\# F90 = ifort/' Makefile
+fi    
+
+cd ../calchep
 make
 make
 cd ../micromegas
