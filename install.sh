51770f66c03d4c07dcf1509e8046f6955b8db4bf
Diego Restrepo <restrepo@udea.edu.co>
2017-10-11 23:59:15 -0500

    alpha version of install script

--- install.sh
+++ install.sh
@@ -11,3 +11,29 @@ make
 cd ../micromegas
 make
 make
+
+# Install generaed codes in several tools
+MODELp=$(git branch | sed -r 's/^\*\s+//')
+#TODO: Check against SARAH/Models DIRs
+if [ "$MODELp" != master ];then
+    MODELDIR=$(echo $MODELp | awk -F'+' '{print $1}' )
+    MODEL=$(echo $MODELp | awk -F'+' '{print $2}' )
+    if [ "$(echo $MODEL | awk -F'+' '{print $2}' )" ];then
+       echo ERROR: third level models not yet implemented
+       exit
+    fi
+    sep='/'
+    if [ ! $MODEL ];then
+	MODEL=MODELDIR
+	MODELDIR=''
+	sep=''
+    fi
+    if [ ! -d SARAH/Models/$MODELDIR$sep$MODEL ] && [ -d  BSM/SARAH/Models$MODELDIR$sep$MODEL ]; then
+	cp -r BSM/SARAH/Models/$MODELDIR  SARAH/Models
+    fi
+    for tool in  SPHENO micromegas madgraph:
+        if [ ! -d $tool/$MODELDIR$MODEL ] && [ -d  BSM/SPHENO/$MODELDIR$MODEL ]; then
+	    cp -r BSM/SPHENO/$MODELDIR$MODEL $tool
+        fi    		     
+	
+fi
