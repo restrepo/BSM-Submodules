246ee87d9756349f71437774d5ee0f1746f104dd
Diego Restrepo <restrepo@udea.edu.co>
2020-01-22 17:18:14 -0500

    fixed sm test

--- test_sm.sh
+++ test_sm.sh
@@ -10,5 +10,5 @@ make Model=SM
 mkdir -p  ../test_sm
 cd ../test_sm
 ../SPHENO/bin/SPhenoSM ../SPHENO/SM/Input_Files/LesHouches.in.SM
-echo  $(grep -i -A2 'block mass' SPheno.spc.SM  | grep -E '\s+25' | awk '{print $2}') == 1.27270603E+02
+echo  $(grep -i -A30 'block mass' SPheno.spc.SM | grep -E '^\s*[0-9]+\s+[0-9]\.[0-9]+[Ee][+\-][0-9]+\s+\#\s*[A-Za-z0-9_]+$' | grep -E '\s+25' | awk '{print $2}') == 1.27270603E+02
 
