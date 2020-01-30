b872892ea44d6e969842cefb7ff916ad00605571
Diego Restrepo <restrepo@udea.edu.co>
2020-01-30 10:13:24 -0500

    test BSM models changes

--- test_sm.sh
+++ test_sm.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
+if [ ! "$1" ];then
+    MODEL=SM
+else
+    MODEL="$1"
+fi
+
 math << EOI || die
       <<"SARAH/SARAH.m"
-      Start["SM"];
+      Start["$MODEL"];
       MakeSPheno[];
 EOI
-cp -r SARAH/Output/SM/EWSB/SPheno/ SPHENO/SM
+cp -r SARAH/Output/$MODEL/EWSB/SPheno/ SPHENO/$MODEL
 cd SPHENO
-make Model=SM
+make Model=$MODEL
 mkdir -p  ../test_sm
 cd ../test_sm
-../SPHENO/bin/SPhenoSM ../SPHENO/SM/Input_Files/LesHouches.in.SM
-echo  $(grep -i -A30 'block mass' SPheno.spc.SM | grep -E '^\s*[0-9]+\s+[0-9]\.[0-9]+[Ee][+\-][0-9]+\s+\#\s*[A-Za-z0-9_]+$' | grep -E '\s+25' | awk '{print $2}') == 1.27939961E+02
+../SPHENO/bin/SPheno$MODEL ../SPHENO/$MODEL/Input_Files/LesHouches.in.$MODEL
+echo  $(grep -i -A30 'block mass' SPheno.spc.$MODEL | grep -E '^\s*[0-9]+\s+[0-9]\.[0-9]+[Ee][+\-][0-9]+\s+\#\s*[A-Za-z0-9_]+$' | grep -E '\s+25' | awk '{print $2}') == 1.27939961E+02
 
