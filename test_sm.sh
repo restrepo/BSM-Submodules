a00e2430e66f7208ae9fc2ce80a9280342a39339
Diego Restrepo <restrepo@udea.edu.co>
2020-01-30 20:22:40 -0500

    lc

--- test_sm.sh
+++ test_sm.sh
@@ -15,6 +15,13 @@ cd SPHENO
 make Model=$MODEL
 mkdir -p  ../test_sm
 cd ../test_sm
+if [ $MODEL == SM ];then
+    mh=1.27939961E+02
+elif [ $MODEL == SSDM ]; then
+    mh=1.30287679E+02
+else
+    mh=1.27939961E+02
+fi
 ../SPHENO/bin/SPheno$MODEL ../SPHENO/$MODEL/Input_Files/LesHouches.in.$MODEL
-echo  $(grep -i -A30 'block mass' SPheno.spc.$MODEL | grep -E '^\s*[0-9]+\s+[0-9]\.[0-9]+[Ee][+\-][0-9]+\s+\#\s*[A-Za-z0-9_]+$' | grep -E '\s+25' | awk '{print $2}') == 1.27939961E+02
+echo  $(grep -i -A30 'block mass' SPheno.spc.$MODEL | grep -E '^\s*[0-9]+\s+[0-9]\.[0-9]+[Ee][+\-][0-9]+\s+\#\s*[A-Za-z0-9_]+$' | grep -E '\s+25' | awk '{print $2}') == $mh
 
